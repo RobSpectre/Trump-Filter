465583daca72c38d3682d5a094e481ea275ce21e
Rob Spectre <rob.spectre@gmail.com>
2011-05-15 20:19:07 -0400

    1) Added options page - users can select three different filter settings. 2) Added README documentation. 3) Added Google Analytics integration. 4) Fixed background message passing.

---
+++ README.md
@@ -0,0 +1,54 @@
+Jeter Filter
+================================
+A Chrome Extension that removes all the Number Two from your Internet.
+
+
+Overview
+--------------------------
+Every day, millions of people all around the world browse the Internet at terrible risk to their emotional and psychological well being.
+  
+
+
+Features
+--------------------------
+
+* Detects instances of Derek Jeter on web pages, warning users of the horror they may contain.
+* Attempts to identify semantically the part of the web page with the reference and remove it.
+* Checks for any remaining visual elements (e.g. images, Flash) that contain Derek Jeter and remove them. 
+* Logs URIs containing Derek Jeter on FluidInfo in an effort to make filtering Derek Jeter easier for app developers worldwide.
+
+
+Usage
+--------------------------
+Simple install the Chrome Extension from the Chrome Web Store here:
+
+Then surf the web, comforted by the protection you are now afforded.
+
+
+Installation (for Developers)
+-------------------------
+Want to hack on the Jeter Filter?  Sweet!
+
+Here's a howto to get the source of the Chrome Extension running in your environment.
+
+1) Grab latest source
+<pre>
+git clone git@github.com:RobSpectre/Jeter-Filter.git
+</pre>
+
+2) Enable Chrome Extension in your Chrome install
+<pre>
+Open Chrome.
+Navigate to Settings... Tools... Extensions.
+Click + on "Developer mode"
+Click "Load unpacked extensions..."
+Navigate to directory you installed Jeter Filter and click Open.
+Chuckle to self as you reload this page and significant chunks of it suddenly disappear.
+</pre>
+
+3) Make the world a safer place.
+
+
+About
+-------------------------
+This work is licensed GPLv3.  Created by Rob Spectre.  Lovingly crafted in Brooklyn, NY.
--- filter.js
+++ filter.js
@@ -1,11 +1,53 @@
-var regex = /Jeter/;
+/*
+ * Jeter Filter - Content Script
+ * 
+ * This is the primary JS file that manages the detection and filtration of Number Two from the web page.
+ */
 
+// Variables
+var regex = /Jeter/;
 var search = regex.exec(document.body.innerText);
 
+
+// Functions
+function filterMild() {
+	console.log("Filtering Number Two with Vindictive filter...");
+	return $(":contains('Jeter'):not('body'):not('html'):not('div')");
+}
+
+function filterDefault () {
+	console.log("Filtering Number Two with Default filter...");
+	return $(":contains('Jeter'):not('body'):not('html'):not('div')").closest('div');
+}
+
+function filterVindictive() {
+	console.log("Filtering Number Two with Vindictive filter...");
+	return $(":contains('Jeter'):only-child:last-child:not('body'):not('html')");
+}
+
+function getElements(filter) {
+   if (filter == "mild") {
+	   return filterMild();
+   } else if (filter == "vindictive") {
+	   return filterVindictive();
+   } else {
+	   return filterDefault();
+   }
+}
+
+function filterElements(elements) {
+	console.log("Elements to filter: ", elements);
+	elements.fadeOut("fast");
+}
+
+
+// Implementation
 if (search) {
-   console.log("No. 2 found on page! - Searching for elements...");
-   divs = $(":contains('Jeter'):not('body'):not('html'):not('div')").closest('div');
-   console.log("Divs found: ", divs);
-   divs.fadeOut("fast");
+   console.log("Number Two found on page! - Searching for elements...");
+   chrome.extension.sendRequest({method: "loadFilter"}, function(response) {
+	   console.log("Filter setting stored is: " + response.filter);
+	   elements = getElements(response.filter);
+	   filterElements(elements);
+	 });
   chrome.extension.sendRequest({}, function(response) {});
 }
