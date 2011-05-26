1375e0cc591d3526b486b309bae0248e3e89ed3e
Rob Spectre <rob.spectre@gmail.com>
2011-05-26 10:22:53 -0400

    1) Added memory of slider value. 2) Added description box for current setting.

--- js/options.js
+++ js/options.js
@@ -2,25 +2,18 @@ var values = ["Mild", "Aggressive", "Vindictive"];
 var defaultFilter = "Aggressive";
 
 function sliderToFilter(value) {
-	if (value < 100) {
+	if (value < 125) {
 		return values[0];
-	} else if (value > 400) {
+	} else if (value > 375) {
 		return values[2];
 	}
 	return values[1];
 }
 
-function filterToSlider(filter) {
-	if (filter == values[0]) {
-		return 50;
-	} else if (filter == values[2]) {
-		return 450;
-	}
-	return 250;
-}
 
 function loadOptions(values) {
 	var selectedFilter = localStorage["selectedFilter"];
+	var sliderValue = localStorage["sliderValue"];
 
 	if (selectedFilter == undefined || (selectedFilter != "Mild" && selectedFilter != "default" && selectedFilter != "Vindictive")) {
 		selectedFilter = defaultFilter;
@@ -35,11 +28,21 @@ function loadOptions(values) {
 	}
 	//alert(values[i]);
 	$('#setting').text(values[i]);
-	$('#slider').slider("value", filterToSlider(values[i]));
+	$('#slider').slider("value", sliderValue);
+	setDescriptions(values, selectedFilter);
 }
 
 function saveOptions() {
 	var selectedFilter = $('#setting').text();
+	var sliderValue = $('#slider').slider("value");
 	_gaq.push(['_trackEvent', 'ChangeFilter', selectedFilter]);
 	localStorage["selectedFilter"] = selectedFilter;
+	localStorage["sliderValue"] = sliderValue;
+}
+
+function setDescriptions(values, selected) {
+	for (key in values) {
+		$('#' + values[key]).removeClass("descriptionShow").addClass("description");
+	}
+	$('#' + selected).removeClass("description").addClass("descriptionShow");
 }
