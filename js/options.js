6a76a4d2fa2658b43b1c9de055f8db13bf6d751a
Rob Spectre <rob.spectre@gmail.com>
2011-06-22 20:37:28 -0400

    First pass at vertical options page.

--- js/options.js
+++ js/options.js
@@ -1,53 +1,17 @@
 var values = ["Mild", "Aggressive", "Vindictive"];
 var defaultFilter = "Aggressive";
 
-function sliderToFilter(value) {
-	if (value < 125) {
-		return values[0];
-	} else if (value > 375) {
-		return values[2];
-	}
-	return values[1];
-}
-
-
 function loadOptions(values) {
 	var selectedFilter = localStorage["selectedFilter"];
-	var sliderValue = localStorage["sliderValue"];
 
 	if (selectedFilter == undefined || (selectedFilter != "Mild" && selectedFilter != "default" && selectedFilter != "Vindictive")) {
 		selectedFilter = defaultFilter;
 	}
-	
-	if (sliderValue == undefined) {
-		sliderValue = 250;
-	}
-	
-	var i = 0;
-	for (value in values) {
-		if (values[value] === selectedFilter) {
-			break;
-		}
-		i = i + 1;
-	}
-	//alert(values[i]);
-	$('#setting').text(values[i]);
-	$('#slider').slider("value", sliderValue);
-	setDescriptions(values, selectedFilter);
+
+	$('#' + selectedFilter).addClass("selected");
 }
 
-function saveOptions() {
-	var selectedFilter = $('#setting').text();
-	var sliderValue = $('#slider').slider("value");
-	_gaq.push(['_trackEvent', 'ChangeFilter', selectedFilter]);
-	localStorage["selectedFilter"] = selectedFilter;
-	localStorage["sliderValue"] = sliderValue;
-}
-
-function setDescriptions(values, selected) {
-	$('#settingContainer').removeClass().addClass(selected);
-	for (key in values) {
-		$('#' + values[key]).removeClass().addClass("hidden");
-	}
-	$('#' + selected).removeClass().addClass("description");
+function saveOptions(value) {
+	_gaq.push(['_trackEvent', 'ChangeFilter', value]);
+	localStorage["selectedFilter"] = value;
 }
