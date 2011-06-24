96663249e0583972fbc6988b33adcef6c8d379b2
Rob Spectre <rob.spectre@gmail.com>
2011-06-23 23:58:53 -0400

    Final pass on options page. Simplified onclick events for settings. Added CSS for selected.

--- js/options.js
+++ js/options.js
@@ -1,17 +1,24 @@
 var values = ["Mild", "Aggressive", "Vindictive"];
 var defaultFilter = "Aggressive";
 
-function loadOptions(values) {
+function loadOptions() {
 	var selectedFilter = localStorage["selectedFilter"];
 
 	if (selectedFilter == undefined || (selectedFilter != "Mild" && selectedFilter != "default" && selectedFilter != "Vindictive")) {
 		selectedFilter = defaultFilter;
 	}
+	
+	$('#' + selectedFilter).addClass('selected');
 
-	$('#' + selectedFilter).addClass("selected");
-}
-
-function saveOptions(value) {
-	_gaq.push(['_trackEvent', 'ChangeFilter', value]);
-	localStorage["selectedFilter"] = value;
+	// Populate stats
+	var pageCount = localStorage["pages"];
+	var jeterCount = localStorage["jeters"];		
+	if (isNaN(parseInt(pageCount)) || isNaN(parseInt(jeterCount))) {
+		pageCount = 0;
+		jeterCount = 0;
+		localStorage["pages"] = 0;
+		localStorage["jeters"] = 0;
+	}
+	$('#pagecount').text(pageCount);
+	$('#jetercount').text(jeterCount);
 }
