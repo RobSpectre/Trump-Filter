46d83435cf6ab214076b12d8606484a47a45e67d
Jamie Farrelly <jamie.farrelly@paywithfire.com>
2016-07-12 19:32:15 +0100

    removing console logs

--- js/background.js
+++ js/background.js
@@ -1,7 +1,5 @@
 function onMessage(request, sender, sendResponse) {
   if (request.method == "saveStats") { 
-    console.log("Storing stats...");
-    console.log ("Adding " + request.pokemons + " pokemons to stats.");
     chrome.storage.sync.get({
       pokemons: 0,
       pages: 0
@@ -14,11 +12,7 @@ function onMessage(request, sender, sendResponse) {
     sendResponse({});
   } else {
     // Show icon
-    console.log("Putting badge on address bar.");
     chrome.pageAction.show(sender.tab.id);
-
-    // Log event with Google Analytics
-    console.log("Logging Filter event...");
     chrome.storage.sync.get({
       filter: 'aggro'
     });
--- js/filter.js
+++ js/filter.js
@@ -11,17 +11,14 @@ var search = regex.exec(document.body.innerText);
 
 // Functions
 function filterMild() {
-	console.log("Filtering pokemon with Mild filter...");
 	return $(":contains('POKEMON'), :contains('Pokemon'), :contains('pokemon'), :contains('Pokémon, :contains('pokémon')").filter("h1,h2,h3,h4,h5,p,span,li");
 }
 
 function filterDefault () {
-	console.log("Filtering pokemon with Default filter...");
 	return $(":contains('POKEMON'), :contains('Pokemon'), :contains('pokemon), :contains('Pokémon, :contains('pokémon')").filter(":only-child").closest('div');
 }
 
 function filterVindictive() {
-	console.log("Filtering pokemon with Vindictive filter...");
 	return $(":contains('POKEMON'), :contains('Pokemon'), :contains('pokemon'), :contains('Pokémon, :contains('pokémon')").filter(":not('body'):not('html')");
 }
 
@@ -36,22 +33,18 @@ function getElements(filter) {
 }
 
 function filterElements(elements) {
-	console.log("Elements to filter: ", elements);
 	elements.fadeOut("fast");
 }
 
 
 // Implementation
 if (search) {
-   console.log("pokemon found on page! - Searching for elements...");
    chrome.storage.sync.get({
      filter: 'aggro',
    }, function(items) {
-	   console.log("Filter setting stored is: " + items.filter);
 	   elements = getElements(items.filter);
 	   filterElements(elements);
 	   chrome.runtime.sendMessage({method: "saveStats", pokemons: elements.length}, function(response) {
-			  console.log("Logging " + elements.length + " pokemons."); 
 		 });
 	 });
   chrome.runtime.sendMessage({}, function(response) {});
