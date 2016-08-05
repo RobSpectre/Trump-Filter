4aae930ef8ce83af2af92bce9c13e9e0ffbce1be
Chris Sparshott <idioteque23@gmail.com>
2016-08-05 19:43:17 +0100

    code intent

--- js/filter.js
+++ js/filter.js
@@ -10,60 +10,42 @@ var search = regex.exec(document.body.innerText);
 var selector = ":contains('POKEMON'), :contains('Pokemon'), :contains('pokemon'), :contains('Pokémon'), :contains('pokémon')";
 
 // Functions
-function filterMild()
-{
-  return $(selector).filter("h1,h2,h3,h4,h5,p,span,li");
+function filterMild() {
+	return $(selector).filter("h1,h2,h3,h4,h5,p,span,li");
 }
 
-function filterDefault()
-{
-  return $(selector).filter(":only-child").closest('div');
+function filterDefault () {
+	return $(selector).filter(":only-child").closest('div');
 }
 
-function filterVindictive()
-{
-  return $(selector).filter(":not('body'):not('html')");
+function filterVindictive() {
+	return $(selector).filter(":not('body'):not('html')");
 }
 
-function getElements(filter)
-{
-  if(filter == "mild")
-  {
-    return filterMild();
-  }
-  else if(filter == "vindictive")
-  {
-    return filterVindictive();
-  }
-  else
-  {
-    return filterDefault();
-  }
+function getElements(filter) {
+   if (filter == "mild") {
+	   return filterMild();
+   } else if (filter == "vindictive") {
+	   return filterVindictive();
+   } else {
+	   return filterDefault();
+   }
 }
 
-function filterElements(elements)
-{
-  elements.fadeOut("fast");
+function filterElements(elements) {
+	elements.fadeOut("fast");
 }
+
 
 // Implementation
-if(search)
-{
-  chrome.storage.sync.get(
-    {filter: 'aggro'},
-    function (items)
-    {
-      var elements = getElements(items.filter);
-
-      filterElements(elements);
-      chrome.runtime.sendMessage(
-        {
-          method:   "saveStats",
-          pokemons: elements.length
-        },
-        function (response) { }
-      );
-    }
-  );
-  chrome.runtime.sendMessage({}, function (response) {});
+if (search) {
+   chrome.storage.sync.get({
+     filter: 'aggro'
+   }, function(items) {
+	   var elements = getElements(items.filter);
+	   filterElements(elements);
+	   chrome.runtime.sendMessage({method: "saveStats", pokemons: elements.length}, function(response) {
+		 });
+	 });
+  chrome.runtime.sendMessage({}, function(response) {});
 }
